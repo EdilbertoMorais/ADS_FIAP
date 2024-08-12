94a1bec7c08988c8a5abf1264c601da08cb7ff49
Edilberto Morais <edilbertocmorais@gmail.com>
2024-08-12 16:03:32 -0300

    add fase 5

---
+++ Java/Nivelamento/Fase_5/Exemplos.java
@@ -0,0 +1,9 @@
+package com.fiap.Fase_5;
+
+public class Exemplos {
+    public static void main(String[] args) {
+
+        int resultado = (((10 + 3) / 2) * 5) % 3;
+        System.out.println(STR."Resultado: \{resultado}");
+    }
+}
